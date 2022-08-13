7f43a073afd34adcc4e7a4428a0763e9ce00760f
minhnhatphan <nminh238@gmail.com>
2022-08-13 14:58:07 +0700

    refactor'

--- api/common/constants.py
+++ api/common/constants.py
@@ -1,1 +1,5 @@
 EXCEL_FILE_EXTENSIONS = ['xls', 'xlsx']
+NATIONALITY = {
+    "native" :['Anh', 'Ai-len', 'Mỹ', 'Ai-Len', 'Nam Phi', 'Úc', 'Đan Mạch', 'Ailen', 'Ấn Độ', 'Hungari', 'Canada', 'Italy', 'Thuỵ Điển', 'Ý'],
+    "non_native":['Ai Cập', 'Thổ Nhĩ Kỳ', 'Pháp', 'Ukraine', 'Hy Lạp', 'Brasil', 'Morocco', 'Croatia', 'Russia', 'Ba Lan', 'Nga', 'Zimbabwean',  'Colombia']
+}
--- api/resources/services/matchService.py
+++ api/resources/services/matchService.py
@@ -1,17 +1,10 @@
 from common.util import readDataframeFrombase64, zipDataFrames
-from resources.services.geocodingService import create_GV_DataFrame
+from common.constants import *
+
+from resources.services.geocodingService import get_coordinates
 import numpy as np
-
-
-def match(school_data, teacher_data) -> bytes:
-    df_school = readDataframeFrombase64(school_data['data'])
-    df_teacher = readDataframeFrombase64(teacher_data['data'])
-
-    df_school = create_Truong_DataFrame(df_school)
-    df_teacher = create_GV_DataFrame(df_teacher)
-
-    print(df_teacher.head())
-    return zipDataFrames([df_school, df_teacher], ["school.xlsx", "teacher.xlsx"])
+import pandas as pd
+import math
 
 
 def create_Truong_DataFrame(df):
@@ -24,6 +17,63 @@ def create_Truong_DataFrame(df):
     tietTrongNgay = [1, 2, 3, 4, 5, 6, 7, 8]
     newdf['Tiet_Trong_Ngay'] = np.tile(tietTrongNgay, len(
         newdf)//len(tietTrongNgay) + 1)[:len(newdf)]
-    finaldf = newdf.sort_values(by=['Ten Truong', 'Khoi', 'Lop so'], axis=0, ascending=[False, True, True], inplace=False,
-                                kind='quicksort', na_position='first', ignore_index=True, key=None)
-    return finaldf
+
+    return newdf.sort_values(by=['Ten Truong', 'Khoi', 'Lop so'], axis=0, ascending=[False, True, True], inplace=False,
+                             kind='quicksort', na_position='first', ignore_index=True, key=None)
+
+
+def isTeacherNativeNation(nation: str) -> bool:
+    if nation in NATIONALITY["native"]:
+        return True
+
+    if nation in NATIONALITY["non_native"]:
+        return False
+
+    return None
+
+
+def rankTeacher(year_exp, native, is_new_teacher):
+    if not isinstance(year_exp, str) and not math.isnan(year_exp, ):
+        if int(year_exp) >= 2 and native and not is_new_teacher:
+            return 'A'
+        if int(year_exp) >= 1 and not is_new_teacher:
+            return 'B'
+
+    return 'C'
+
+
+def create_GV_DataFrame(df):
+    # create dataframe
+    giao_vien_chi_tiet = pd.DataFrame(columns=[
+                                      'Ten GV', 'Quoc Tich', 'English Native Speaker', 'Tham Nien', 'Dia Chi', 'Giao Vien Moi', 'Nhom'])
+
+    # Extract info from excel file
+    for i in range(2, df.shape[0]):
+        is_new_teacher = isinstance(df.iloc[i, 20], str)
+        native = isTeacherNativeNation(df.iloc[i, 2])
+        group = rankTeacher(df.iloc[i, 5], native, is_new_teacher)
+
+        giao_vien_chi_tiet.loc[len(giao_vien_chi_tiet.index)] = [
+            df.iloc[i, 1], df.iloc[i, 2], native, df.iloc[i, 5], df.iloc[i, 9], is_new_teacher, group]
+
+    # Handling NaN values
+    giao_vien_chi_tiet = giao_vien_chi_tiet[giao_vien_chi_tiet['Quoc Tich'].notna(
+    )]
+
+    # Retrieving Coordinates
+    giao_vien_chi_tiet[['Latitude', 'Longitude']
+                       ] = giao_vien_chi_tiet['Dia Chi'].apply(get_coordinates)
+
+    return giao_vien_chi_tiet
+
+
+def match(school_data, teacher_data) -> bytes:
+    df_school = readDataframeFrombase64(school_data['data'])
+    df_teacher = readDataframeFrombase64(teacher_data['data'])
+
+    df_school = create_Truong_DataFrame(df_school)
+    df_teacher = create_GV_DataFrame(df_teacher)
+
+    print(df_school.head())
+    print(df_teacher.head())
+    return zipDataFrames([df_school, df_teacher], ["school.xlsx", "teacher.xlsx"])
